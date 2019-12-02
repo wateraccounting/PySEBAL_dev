52b1a4e0c2f6ce018e59612e850610defe25b75e
spareeth <spareeth@gmail.com>
2019-12-02 22:47:10 +0100

    added python 3 support only for Landsat images

--- SEBAL/Run_py2.py
+++ SEBAL/Run_py2.py
@@ -5,14 +5,14 @@ Created on Tue May 03 13:12:18 2016
 @author: tih
 """
 
-import SEBAL
+import pysebal_py2
 
-inputExcel = r"D:\PySebal\SEBAL-master_3.3.7.1\SEBAL-master\SEBAL\InputEXCEL_v3_3_7_probav.xlsx"
+inputExcel = r"D:\PySebal\PySEBAL_Pareeth\PySEBAL_dev\docs\InputEXCEL_v3_3_7.xlsx"
 
 for number in range(2,3):
     try:
-		print 'starting line num: %d' % number
-        SEBAL.SEBALcode(number,inputExcel)
+        print 'starting line num: %d' % number
+        pysebal_py2.SEBALcode(number,inputExcel)
         print 'line num: %d done' % number
         
     except:
---
+++ SEBAL/Run_py3.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 03 13:12:18 2016
+
+@author: tih
+"""
+
+import pysebal_py3
+
+inputExcel = r"D:\PySebal\PySEBAL_Pareeth\PySEBAL_dev\docs\InputEXCEL_v3_3_7.xlsx"
+
+for number in range(2,3):
+    try:
+        print ('starting line num: %d' % number)
+        pysebal_py3.SEBALcode(number,inputExcel)
+        print ('line num: %d done' % number)
+        
+    except:
+        print ('SEBAL did not run line %d fully' % number)
+        
+
