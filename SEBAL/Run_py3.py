9ab724e8fda1d79c314e0f9dec9809bcc00fa3a1
spareeth <spareeth@gmail.com>
2020-02-06 12:10:59 +0100

    minor cleaning

--- SEBAL/Run_py3.py
+++ SEBAL/Run_py3.py
@@ -9,9 +9,9 @@ import pysebal_py3
 ##### USER INPUTS
 
 ##### For Linux SET THE PATH TO INPUT EXCEL SHEET #####
-inputExcel = r"/mnt/d/PySEBAL/PySEBAL_Pareeth/PySEBAL_dev/docs/InputEXCEL_v3_3_7_LIN.xlsx"
+#inputExcel = r"/mnt/d/PySEBAL_dev/docs/InputEXCEL_v3_3_7_LIN.xlsx"
 ##### For Windows SET THE PATH TO INPUT EXCEL SHEET #####
-# inputExcel = r"D:\PySebal\PySEBAL_Pareeth\PySEBAL_dev\docs\InputEXCEL_v3_3_7_WIN.xlsx"
+inputExcel = r"D:\PySEBAL_dev\docs\InputEXCEL_v3_3_7_WIN.xlsx"
 st = 2 # starting row number
 en = 2 # ending row number
 
