47ebb5eba64cd82a3b82810a8fd16d45ccfb7c4d
Sajid <spareeth@gmail.com>
2021-07-31 13:30:37 +0200

    new branch working with osgeo4w v1

--- gapfilling/gapfill_env.bat
+++ gapfilling/gapfill_env.bat
@@ -1,5 +1,3 @@
-set GISBASE=C:\OSGeo4W\apps\grass\grass78
-set GRASSBIN=C:\OSGeo4W\apps\grass\grass78\grass78
-set GISRC=C:\OSGeo4W\apps\grass\grass78\demolocation\.grassrc78
-set PROJ_LIB=C:\OSGeo4W\apps\grass\grass78\share\proj
-set PATH=%PATH%;C:\OSGeo4W\apps\grass\grass78\lib;C:\OSGeo4W\apps\grass\grass78\bin;C:\OSGeo4W\apps\grass\grass78\scripts;C:\OSGeo4W\apps\grass\grass78\share
+set GRASSBIN=C:\OSGeo4W64\bin\grass78
+set GISRC=C:\OSGeo4W64\apps\grass\grass78\demolocation\.grassrc78
+set PATH=%PATH%;C:\OSGeo4W64\apps\grass\grass78\lib;C:\OSGeo4W64\apps\grass\grass78\bin;C:\OSGeo4W64\apps\grass\grass78\scripts
