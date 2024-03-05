9d33b8440605ea7ba0930c879b95af994811859c
Sajid <spareeth@gmail.com>
2024-03-05 21:26:47 +0100

    changed the language to en in config.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,14 +19,14 @@
 
 # -- Project information -----------------------------------------------------
 
-project = u'PySEBAL_doc'
-copyright = u'2019, Sajid Pareeth'
+project = u'PySEBAL'
+copyright = u'2024, Sajid Pareeth'
 author = u'Sajid Pareeth'
 
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.2'
+release = u'4'
 
 
 # -- General configuration ---------------------------------------------------
@@ -62,7 +62,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
