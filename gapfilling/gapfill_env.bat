9f74e6435b3f922dcab7be798edc1d81345f62e2
Sajid <spareeth@gmail.com>
2024-03-07 09:25:01 +0100

    changed the grass version to 8.3 in gapfilling

--- gapfilling/gapfill_env.bat
+++ gapfilling/gapfill_env.bat
@@ -1,3 +1,3 @@
-set GRASSBIN=C:\OSGeo4W\bin\grass78
-set GISRC=C:\OSGeo4W\apps\grass\grass78\demolocation\.grassrc78
-set PATH=%PATH%;C:\OSGeo4W\apps\grass\grass78\lib;C:\OSGeo4W\apps\grass\grass78\bin;C:\OSGeo4W\apps\grass\grass78\scripts
+set GRASSBIN=C:\OSGeo4W\bin\grass83
+set GISRC=C:\OSGeo4W\apps\grass\grass83\demolocation\.grassrc83
+set PATH=%PATH%;C:\OSGeo4W\apps\grass\grass83\lib;C:\OSGeo4W\apps\grass\grass83\bin;C:\OSGeo4W\apps\grass\grass83\scripts
